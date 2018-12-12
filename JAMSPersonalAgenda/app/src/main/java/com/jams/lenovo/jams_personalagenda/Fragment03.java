c7031277b64786c93d46d32c2183747aca02020f
FannyLorenzoQ <fanny.llq@gmail.com>
2018-12-11 20:19:11 -0500

    Codigo de aplicativo en android - JAMS - Es una agenda personal

---
+++ JAMSPersonalAgenda/app/src/main/java/com/jams/lenovo/jams_personalagenda/Fragment03.java
@@ -0,0 +1,22 @@
+package com.jams.lenovo.jams_personalagenda;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment03 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment03, container, false);
+
+    }
+
+
+}
